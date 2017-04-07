282e44f33f262cb42c7bf52a75abd73f682dbdf3
Tennyson Holloway <tennyson.holloway@gmail.com>
2017-04-07 16:17:57 -0700

    added plugin header

---
+++ wpspin.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Plugin Name: Spinitron Integration
+Description: An deep wordpress and spinitron integration that allows DJs to create and edit content
+Version:     20170407
+Author:      KSDT Radio
+Author URI:  https://github.com/ksdt/wpspin
+License:     MIT
+License URI: https://github.com/ksdt/wpspin/blob/master/LICENSE
+Text Domain: wporg
+*/
+
+
+?>
